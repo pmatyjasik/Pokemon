16c8452852797cdb944b367e4108e4b4abde1aa1
Piotr Matyjasik <pmatyjasik@icloud.com>
2024-08-23 20:34:30 +0200

    fix: cache (#12)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,9 @@
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const getCurrentDate = async () => {
-    const response = await fetch("https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Warsaw");
+    const response = await fetch("https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Warsaw", {
+        next: { revalidate: 60 },
+    });
     const data = await response.json();
     const date = new Date(data.dateTime);
     const day = date.getDate().toString().padStart(2, "0");
